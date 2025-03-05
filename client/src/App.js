f9404a9b1e6e37232d31d67592779a72ad72a908
mohammadabk2 <mohammad294294@gmail.com>
2025-03-05 16:53:27 +0200

    adds dynamic react components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import components here
 import Home from "./pages/Home";
+import DynamicInput from "./components/InputComponent";
+import DynamicButton from "./components/ButtonComponent";
+
 // TODO:
 // import About from './pages/About';
 // import Settings from './pages/Settings';
-let username, password;
 
 function App() {
+  const [username, setUserName] = useState("");
+  const [password , setPassword] = useState("");
+
+  const handleUserName = (event) =>{
+    setUserName(event.target.value);
+  }
+
+  const handlePassword = (event) =>{
+    setPassword(event.target.value);
+  }
+
+  const signIn = (event) => {
+    console.log("sign in button clicked");
+    console.log(username);
+    console.log(password);
+  };
+
+  const signUp = (event) => {
+    console.log("sign up button clicked");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Beit Hanina Community Center Volunteer App</h1>
       </header>
       <main>
-
         {/* //TODO: add routes as needed*/}
         {/* <Switch> */}
-          {/* <Route path="/" exact component={Home} />
+        {/* <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/settings" component={Settings} /> */}
-          {/* Add other routes here */}
+        {/* Add other routes here */}
         {/* </Switch> */}
-        
 
         <div className="Sign-in-box">
           <h2>Welcome to Beit Hanina Community Center</h2>
           <div className="Input-field-box">
-            <input className="Input-field" placeholder="Username" type="text" value={username}></input>
-            <input className="Input-field" placeholder="Password" type="password" value={password}></input>
+            <DynamicInput
+              className="Input-field"
+              type="text"
+              value={username}
+              onChange={handleUserName}
+              placeholder="Enter UserName"
+            />
+            <DynamicInput
+              className="Input-field"
+              type="password"
+              value={password}
+              onChange={handlePassword}
+              placeholder="Enter Password"
+            />
           </div>
+
           <div className="Button-box">
-            <button className="Button">Sign in</button>
-            <button className="Button">Sign up</button>
+            <DynamicButton className="Button" onClick={signIn} text="Sign in" />
+            <DynamicButton className="Button" onClick={signUp} text="Sign up" />
           </div>
         </div>
       </main>
