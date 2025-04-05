8ba6a15ba9c5979a8814e8a0a7c5832bda972d59
jad <jadosh2000@gmail.com>
2025-04-05 17:43:53 +0300

    fixes personal area box, removes skills

--- client/src/pages/PersonalAreaPage/PersonalAreaVolunteer.js
+++ client/src/pages/PersonalAreaPage/PersonalAreaVolunteer.js
@@ -3,15 +3,12 @@ import { useTranslation } from "react-i18next";
 
 import NavigationBar from "../../components/NavigationBar";
 import DynamicButton from "../../components/ButtonComponent";
-import DropDownMenu from "../../components/DropDownMenu";
 import ManageAccountBox from "../../components/ManageAccountBox";
-import { useSkillOptions } from "../../config/options/Skills";
+import SelectSkills from "../../components/SelectComponent"
 
 const PersonalArea = () => {
   const { t } = useTranslation("personalVolunteer");
-  const { t: tskill } = useTranslation("skills");
   const { t: tsignup } = useTranslation("signUp");
-  const skillsOptions = useSkillOptions();
 
   //TODO change all these to read from database
   const name = "john doe";
@@ -20,31 +17,13 @@ const PersonalArea = () => {
   const approvedHours = 10;
   const unapprovedHours = 1;
 
-  const [userSkills, setUserSkills] = useState([
+  const [userSkills] = useState([
     //TODO call to get user skills
   ]);
 
-  const handleAddSkill = (value) => {
-    //TODO call database and check
-    if (!userSkills.some((skill) => skill.value === value)) {
-      console.log("skill added");
-      setUserSkills([...userSkills, { label: value, value }]);
-      //TODO change to call database and remove the skill
-    } else {
-      console.log("skill already there");
-    }
-  };
+  const handleSkills = () => {
 
-  const handleRemoveSkill = (value) => {
-    //TODO call database and check
-    if (userSkills.some((skill) => skill.value === value)) {
-      console.log("skill removed");
-      // Update the state by filtering out the removed skill
-      setUserSkills(userSkills.filter((skill) => skill.value !== value));
-    } else {
-      console.log("skill not there");
-    }
-  };
+  }
 
   const printToPdf = () => {
     console.log("print to PDF button clicked");
@@ -53,57 +32,38 @@ const PersonalArea = () => {
   return (
     <div className="app flex-box flex-column">
       <NavigationBar />
-      <div className="general-box scroll-box1">
-        <div className="general-box flex-box flex-column smooth-shadow-box">
-          <div className="basic-box-padding">
-            <div className="personal-area-content basic-item-padding">
-              {tsignup("fullName")}: {name}
-            </div>
-
-            <div className="personal-area-content basic-item-padding">
-              {t("user_name")}: {userName}
-            </div>
-
-            <div className="personal-area-content basic-item-padding">
-              {t("approved_hours")}: {approvedHours}
-            </div>
-
-            <div className="personal-area-content basic-item-padding">
-              {t("unapproved_hours")}: {unapprovedHours}
-            </div>
+      <div className="general-box flex-box flex-column smooth-shadow-box">
+        <div className="basic-box-padding">
+          <div className="personal-area-content basic-item-padding">
+            {tsignup("fullName")}: {name}
           </div>
 
-          <div className="flex-box flex-column input-field-box">
-            <div className="personal-area-content">{t("skills")}: </div>
-            {userSkills.map((skill, index) => (
-              <div key={index} className="flex-box">
-                <div>{tskill(skill.label)}</div>
-                <DynamicButton
-                  className="button"
-                  text={tsignup("remove")}
-                  onClick={() => handleRemoveSkill(skill.value)}
-                />
-              </div>
-            ))}
-            <DropDownMenu
-              className="sex-button"
-              text={t("selectskills")}
-              options={skillsOptions.map((skill) => ({
-                label: tskill(skill.label),
-                href: `#${skill.value}`,
-                onClick: () => handleAddSkill(skill.value),
-              }))}
-            />
+          <div className="personal-area-content basic-item-padding">
+            {t("user_name")}: {userName}
           </div>
 
-          <DynamicButton
-            className="button"
-            text={t("to_pdf")}
-            onClick={printToPdf}
-          />
+          <div className="personal-area-content basic-item-padding">
+            {t("approved_hours")}: {approvedHours}
+          </div>
 
-          <ManageAccountBox />
+          <div className="personal-area-content basic-item-padding">
+            {t("unapproved_hours")}: {unapprovedHours}
+          </div>
         </div>
+
+        <SelectSkills
+          type="skills"
+          onChange={handleSkills}
+          choosen={userSkills}
+        />
+
+        <DynamicButton
+          className="button"
+          text={t("to_pdf")}
+          onClick={printToPdf}
+        />
+
+        <ManageAccountBox />
       </div>
     </div>
   );
