c9367611a67d37e141176cc613c1a1d46e5c3523
mohammadabk2 <mohammad294294@gmail.com>
2025-05-07 11:30:10 +0300

    fix homeAdmin page styling to be inline with the rest of the project

--- client/src/pages/HomePage/HomeAdmin.js
+++ client/src/pages/HomePage/HomeAdmin.js
@@ -204,7 +204,7 @@ const HomeAdmin = () => {
   const renderPeople = () => {
     return (
       <>
-        <div className="perosnal-area-content scroll-box1 general-box flex-box flex-column">
+        <div className="scroll-box1 general-box flex-box flex-column">
           <div className="flex-box top-scroll-box1 line-break">
             <DynamicButton
               className="button button-small"
@@ -276,8 +276,8 @@ const HomeAdmin = () => {
   const renderCreateOrg = () => {
     return (
       <>
-        <div className="perosnal-area-content scroll-box1 general-box flex-box flex-column">
-          <div className="flex-box top-scroll-box1">
+        <div className="scroll-box1 general-box flex-box flex-column">
+        <div className="flex-box top-scroll-box1 line-break">
             <div className="flex-box top-scroll-box1 line-break">
               <DynamicButton
                 className="button button-small"
