dd127668267a2b0d3f0b487aea963290064449dd
mohammad quttaineh <mohammadqu@post.jce.ac.il>
2025-05-14 21:45:15 +0300

    Edit ULA.js for testing only(testing Popup)

--- client/src/pages/CommonPages/ULA.js
+++ client/src/pages/CommonPages/ULA.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from "react-i18next";
-
 import NavigationBar from "../../components/layout/NavigationBar";
+import PopupComponent from "../../components/common/PopupComponent";
+import DynamicButton from "../../components/common/ButtonComponent";
 
 const ULAPage = () => {
+  // Remove unused translation hook or use it
   const { t } = useTranslation("copyRight");
+  const [showPopup, setShowPopup] = useState(false);
 
   return (
     <div className="app flex-box flex-column">
-      <NavigationBar /> 
-      <div className="general-box flex-box">
-        <div className="general-box flex-box flex-column smooth-shadow-box basic-box-padding">
-          <div className="bold-text"> {t("ula")} </div>
-          {/* //TODO add to translation legal document */}
-          <div >some legal text</div>
-        </div>
-      </div> 
+      <NavigationBar />
+      
+      {/* Simple Test Section */}
+      <div className="general-box flex-box flex-column smooth-shadow-box basic-box-padding gap-1">
+        <h2 className="bold-text">{t("ula")}</h2> {/* Now using translation */}
+        
+        <DynamicButton
+          className="button button-small"
+          text="Show Test Popup"
+          onClick={() => setShowPopup(true)}
+        />
+
+        <PopupComponent
+          isOpen={showPopup}
+          onClose={() => setShowPopup(false)}
+          message="Test Popup"
+          buttonText="Close"
+        >
+          <p>Simple popup content</p>
+        </PopupComponent>
+      </div>
     </div>
   );
 };
