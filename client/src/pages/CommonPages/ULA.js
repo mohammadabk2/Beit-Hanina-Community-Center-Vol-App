860d56988fa2915a220a3aca7507e7df1fe0869a
mohammad quttaineh <mohammadqu@post.jce.ac.il>
2025-05-19 01:32:23 +0300

    ix: Replace semantic elements with divs and proper image import

--- client/src/pages/CommonPages/ULA.js
+++ client/src/pages/CommonPages/ULA.js
@@ -3,42 +3,52 @@ import { useTranslation } from "react-i18next";
 import NavigationBar from "../../components/layout/NavigationBar";
 import PopupComponent from "../../components/common/PopupComponent";
 import DynamicButton from "../../components/common/ButtonComponent";
+
 const ULAPage = () => {
   const { t } = useTranslation("copyRight");
   const [showPopup, setShowPopup] = useState(false);
+
+  const handlePopupClose = () => {
+    setShowPopup(false);
+  };
 
   return (
     <div className="app flex-box flex-column">
       <NavigationBar />
       
       <div className="general-box flex-box flex-column smooth-shadow-box basic-box-padding gap-1">
-        <h2 className="bold-text">{t("ula")}</h2>
+        <div className="bold-text personal-area-content">{t("ula")}</div>
         
         <DynamicButton
           className="button button-small"
           text="Show Modern Popup"
           onClick={() => setShowPopup(true)}
+          ariaLabel="Open demo popup"
         />
 
         <PopupComponent
           isOpen={showPopup}
-          onClose={() => setShowPopup(false)}
+          onClose={handlePopupClose}
           message="Modern Popup Demo"
           buttonText="Confirm"
-          showCloseButton={false} // Test without close button
-          closeOnOutsideClick={false} // Test click outside behavior
+          showCloseButton={true}
+          closeOnOutsideClick={true}
         >
           <div className="flex-box flex-column">
-            <p className="personal-area-content">New animated popup content!</p>
+            <div className="personal-area-content">popup content</div>
+            
+            {/* ADD THE LOGO IMAGE HERE */}
             <img 
-              src="/demo-image.jpg" 
-              alt="Demo" 
+              src={`${process.env.PUBLIC_URL}/logo192.png`}
+              alt="Community Center Logo" 
               className="preview-img smooth-shadow-box"
             />
+            
           </div>
         </PopupComponent>
       </div>
     </div>
   );
 };
+
 export default ULAPage;
