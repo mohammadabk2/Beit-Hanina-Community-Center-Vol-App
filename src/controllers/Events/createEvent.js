268e9cd3ab3d48e12f9a424d5f061de05db5280e
mohammadabk2 <mohammad294294@gmail.com>
2025-05-21 18:59:25 +0300

    adds token to createEvent.

--- src/controllers/Events/createEvent.js
+++ src/controllers/Events/createEvent.js
@@ -1,5 +1,5 @@
 import dbConnection from "../../database/dbconnection.js";
-import jwt from "jsonwebtoken";
+import validateToken from "../common/validateToken.js";
 
 //TODO validation check for front end
 
@@ -34,68 +34,52 @@ const createEvent = async (req, res) => {
     }
   }
 
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    const message = "Authorization header missing or invalid format.";
-    console.log(message);
-    return res.status(401).send({
-      message: message,
-      status: "fail",
-    });
-  }
-
-  const token = authHeader.split(" ")[1];
-  if (!token) {
-    const message = "Token not provided in the Authorization header.";
-    console.log(message);
-    return res.status(401).send({
-      message: message,
-      status: "fail",
-    });
-  }
-
-  console.log(`Attempting to create new Event from user: ${userID}`);
-
+  let roleType;
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    roleType = await validateToken(req);
+    console.log(`role type: ${roleType.role}`);
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).send({
+      message: error,
+      status: "fail",
+    });
+  }
 
-    if (decoded.id !== userID) {
-      const message = "Unauthorized: Token does not match the requested user.";
-      console.log(message);
-      return res.status(403).send({
-        message: message,
-        status: "fail",
-      });
-    }
+  const roles = ["organizer", "admin"];
+  if (roles.includes(roleType.role)) {
+    console.log(`Attempting to create new Event from user: ${userID}`);
 
-    const answer = await dbConnection.addEvent(
-      userData.eventName,
-      userData.eventDate,
-      userData.eventStartTime,
-      userData.eventEndTime,
-      userData.isActive,
-      userData.orgId,
-      userData.maxNumberOfVolunteers,
-      userData.eventLocation,
-      userData.eventDescription,
-    );
+    try {
+      const answer = await dbConnection.addEvent(
+        userData.eventName,
+        userData.eventDate,
+        userData.eventStartTime,
+        userData.eventEndTime,
+        userData.isActive,
+        userData.orgId,
+        userData.maxNumberOfVolunteers,
+        userData.eventLocation,
+        userData.eventDescription
+      );
 
-    if (answer) {
-      res.status(200).send({
-        message: `Created Event successfully`,
-        status: "success",
-      });
-    } else {
-      res.status(503).send({
-        message: "Database temporarily unavailable. Please try again later.",
+      if (answer) {
+        res.status(200).send({
+          message: `Created Event successfully`,
+          status: "success",
+        });
+      } else {
+        res.status(503).send({
+          message: "Database temporarily unavailable. Please try again later.",
+          status: "error",
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        message: "An internal server error occurred during singUp.",
         status: "error",
       });
     }
-  } catch (error) {
-    res.status(500).send({
-      message: "An internal server error occurred during singUp.",
-      status: "error",
-    });
   }
 };
 export default createEvent;
