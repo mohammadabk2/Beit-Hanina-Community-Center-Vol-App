be451240e510602e265cd2f644420e6b8fc7175e
mohammadabk2 <mohammad294294@gmail.com>
2025-05-21 18:21:54 +0300

    adds token to user actions.

--- src/controllers/Users/actions.js
+++ src/controllers/Users/actions.js
@@ -1,5 +1,5 @@
 import dbConnection from "../../database/dbconnection.js";
-import jwt from "jsonwebtoken"; // Import the jsonwebtoken library
+import validateToken from "../common/validateToken.js";
 
 const userActions = async (req, res) => {
   console.log("approve/reject/log users");
@@ -15,22 +15,14 @@ const userActions = async (req, res) => {
     });
   }
 
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    const message = "Authorization header missing or invalid format.";
-    console.log(message);
-    return res.status(401).send({
-      message: message,
-      status: "fail",
-    });
-  }
-
-  const token = authHeader.split(" ")[1];
-  if (!token) {
-    const message = "Token not provided in the Authorization header.";
-    console.log(message);
-    return res.status(401).send({
-      message: message,
+  let roleType;
+  try {
+    roleType = await validateToken(req);
+    console.log(`role type: ${roleType.role}`);
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).send({
+      message: error,
       status: "fail",
     });
   }
@@ -39,57 +31,49 @@ const userActions = async (req, res) => {
     `Attempting Action ${action} From User: ${userID} on User ${actionID}`
   );
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  const roles = ["volunteer", "organizer", "admin"];
+  if (roles.includes(roleType.role)) {
+    try {
+      let answer;
 
-    if (decoded.id !== userID) {
-      const message = "Unauthorized: Token does not match the requested user.";
-      console.log(message);
-      return res.status(403).send({
-        message: message,
-        status: "fail",
+      if (action === "approve" && actionValue === "NA") {
+        console.log("approving User");
+        answer = await dbConnection.createVolunteer(actionID);
+      }
+
+      if (action === "reject" && actionValue === "NA") {
+        console.log("rejecting User");
+        answer = await dbConnection.rejectUser(actionID);
+      }
+
+      if (action === "log-user") {
+        console.log(`logging about User ${actionValue}`);
+        answer = await dbConnection.addLog(actionID, actionValue);
+      }
+
+      if (!answer) {
+        const message = `action failed!! invalid action type`;
+        console.log(message);
+        res.status(401).send({
+          message: message,
+          status: "fail",
+        });
+      } else {
+        const message = `${action} action successful!`;
+        console.log(message);
+        res.status(200).send({
+          message: message,
+          status: "success",
+          //TODO maybe make the system reload the user table or refresh
+        });
+      }
+    } catch (error) {
+      console.error(`Error during action ${action} error:`, error);
+      res.status(500).send({
+        message: "An internal server error occurred during login.",
+        status: "error",
       });
     }
-
-    let answer;
-
-    if (action === "approve" && actionValue === "NA") {
-      console.log("approving User");
-      answer = await dbConnection.createVolunteer(actionID);
-    }
-
-    if (action === "reject" && actionValue === "NA") {
-      console.log("rejecting User");
-      answer = await dbConnection.rejectUser(actionID)
-    }
-
-    if (action === "log-user") {
-      console.log(`logging about User ${actionValue}`);
-      answer = await dbConnection.addLog(actionID, actionValue);
-    }
-
-    if (!answer) {
-      const message = `action failed!! invalid action type`;
-      console.log(message);
-      res.status(401).send({
-        message: message,
-        status: "fail",
-      });
-    } else {
-      const message = `${action} action successful!`;
-      console.log(message);
-      res.status(200).send({
-        message: message,
-        status: "success",
-        //TODO maybe make the system reload the user table or refresh
-      });
-    }
-  } catch (error) {
-    console.error(`Error during action ${action} error:`, error);
-    res.status(500).send({
-      message: "An internal server error occurred during login.",
-      status: "error",
-    });
   }
 };
 
