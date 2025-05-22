61f0d14a06ef5c19a74a19445452ccb3bf7e7321
jad <jadosh2000@gmail.com>
2025-05-22 16:33:50 +0300

    Changes global background and colors

--- client/src/pages/AboutPage/About.js
+++ client/src/pages/AboutPage/About.js
@@ -11,8 +11,8 @@ const About = () => {
     <div className="app flex-box flex-column">
       {/* //TODO add check if signed in or not and use the right nav bar according to it */}
       <NavigationBar />
-      <div className="general-box scroll-box1 flex-box">
-        <div className="general-box flex-box flex-column smooth-shadow-box">
+      <div className="general-box">
+        <div className="flex-box flex-column smooth-shadow-box">
           {/* <div className="basic-item-padding personal-area-content">
             {tAbout("admins")}
           </div> */}
