e6831eb9e4a8c6a76385b6e1425d30c81b5c3a0b
mohammadabk2 <mohammad294294@gmail.com>
2025-05-22 23:46:25 +0300

    starts work on Events/actions

---
+++ src/controllers/Events/actions.js
@@ -0,0 +1,82 @@
+import dbConnection from "../../database/dbconnection.js";
+import validateToken from "../common/validateToken.js";
+
+const eventActions = async (req, res) => {
+  console.log("Event Actions");
+
+  const { userID, eventID, action, actionValue } = req.body;
+
+  if (!userID || !eventID || !action || !actionValue) {
+    const message = "Request body Failed.";
+    console.log(message);
+    return res.status(400).send({
+      message: message,
+      status: "fail",
+    });
+  }
+
+  let roleType;
+  try {
+    roleType = await validateToken(req);
+    console.log(`role type: ${roleType.role}`);
+  } catch (error) {
+    console.log(error);
+    return res.status(error.statusCode).send({
+      message: error,
+      status: "fail",
+    });
+  }
+
+  console.log(
+    `Attempting Action ${action} From User: ${userID} on Event ${eventID}`
+  );
+
+  const roles = ["volunteer", "organizer", "admin"];
+  if (roles.includes(roleType.role)) {
+    try {
+      let answer ="temp";
+
+      if (roleType.role === "admin") {
+        console.log("Events Admin action");
+        if (action === "approve") {
+          //TODO approve event
+        }
+        if (action === "reject") {
+          //TODO reject event
+        }
+      }
+
+      if (!answer) {
+        const message = `action failed!! invalid action type`;
+        console.log(message);
+        res.status(401).send({
+          message: message,
+          status: "fail",
+        });
+      } else {
+        const message = `${action} action successful!`;
+        console.log(message);
+        res.status(200).send({
+          message: message,
+          status: "success",
+          //TODO maybe make the system reload the Event table or refresh
+        });
+      }
+    } catch (error) {
+      console.error(`Error during action ${action} error:`, error);
+      res.status(500).send({
+        message: "An internal server error occurred during login.",
+        status: "error",
+      });
+    }
+  } else {
+    const message = "User lacks permissions";
+    console.log(message);
+    res.status(401).send({
+      message: message,
+      status: "error",
+    });
+  }
+};
+
+export default eventActions;
