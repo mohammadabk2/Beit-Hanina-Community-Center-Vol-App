c45292e8c79f873b74222cd683ed7a6def6239d0
mohammadabk2 <mohammad294294@gmail.com>
2025-06-19 22:46:57 +0300

    adds link to other projects link

--- client/src/components/layout/CopyRight.js
+++ client/src/components/layout/CopyRight.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 
-
 import DynamicButton from "../common/ButtonComponent";
 import { useNavigate } from "react-router-dom";
 
@@ -10,15 +9,28 @@ const CopyRight = () => {
   const navigate = useNavigate();
 
   const ulaRedirect = () => {
-    console.log("Copy Right Link clicked.")
+    console.log("Copy Right Link clicked.");
     navigate("/ULA");
-  }
+  };
 
+  const mainWebsiteRedirect = () => {
+    console.log("Main Website Link clicked.");
+    window.open("https://google.com", "_blank"); //TODO get link from there if its ready
+  };
   return (
     <>
       <div className="flex-box flex-column basic-box-padding">
         <div className="basic-item-padding">&copy; {t("owner")}</div>
-        <DynamicButton className="link-button" text={t("privacy")} onClick={ulaRedirect} />
+        <DynamicButton
+          className="link-button"
+          text={t("privacy")}
+          onClick={ulaRedirect}
+        />
+        <DynamicButton
+          className="link-button"
+          text={t("website")}
+          onClick={mainWebsiteRedirect}
+        />
       </div>
     </>
   );
