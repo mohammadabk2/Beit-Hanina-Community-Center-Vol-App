ec987bf0bcd485f981e9186b561ed6eb448f6934
mohammadabk2 <mohammad294294@gmail.com>
2025-07-03 17:00:37 +0300

    reverts front-end to point at server.

--- client/src/config/constants/global.js
+++ client/src/config/constants/global.js
@@ -1,2 +1,2 @@
-// export const SERVER_IP = "http://18.192.27.50"; //Pord
-export const SERVER_IP = "http://localhost:5213"; //Testing
+export const SERVER_IP = "http://18.192.27.50"; //Pord
+// export const SERVER_IP = "http://localhost:5213"; //Testing
